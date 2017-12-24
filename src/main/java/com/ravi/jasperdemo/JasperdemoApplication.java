249d191ca99af988d41b7593ce01169e47d0e11f
Sai Lethakula <sailethakula@gmail.com>
2017-12-24 22:16:18 +0530

    made changes to jrxml

--- src/main/java/com/ravi/jasperdemo/JasperdemoApplication.java
+++ src/main/java/com/ravi/jasperdemo/JasperdemoApplication.java
@@ -3,37 +3,41 @@ package com.ravi.jasperdemo;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import com.ravi.jasperdemo.config.JasperReportsSimpleConfig;
 import com.ravi.jasperdemo.controllers.DemoReportExporter;
 import com.ravi.jasperdemo.controllers.DemoReportFiller;
 
 @SpringBootApplication
-public class JasperdemoApplication {
+public class JasperdemoApplication implements CommandLineRunner{
+	
+	@Autowired 
+    private DemoReportFiller demoReportFiller;
+	
+    @Autowired
+    private DemoReportExporter demoReportExporter; 
+
+    @Override
+    public void run(String... args) {
+        demoReportFiller.setReportFileName("statement.jrxml"); 
+        demoReportFiller.compileReport(); 
+        Map<String, Object> parameters = new HashMap<String, Object>(); 
+        parameters.put("title", "Employee Report Example"); 
+        parameters.put("idEmployee", "301101"); 
+        demoReportFiller.setParameters(parameters); 
+        demoReportFiller.fillReport();
+        demoReportExporter.setJasperPrint(demoReportFiller.getJasperPrint()); 
+        demoReportExporter.exportToPdf("statement.pdf", "Ravi chandra"); 
+//        if (args.length > 0 && args[0].equals("exitcode")) { 
+//            throw new ExitException(); 
+//        } 
+
+    } 
 
 	public static void main(String[] args) {
-		//SpringApplication.run(JasperdemoApplication.class, args);
-		
-		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
-        ctx.register(JasperReportsSimpleConfig.class);
-        ctx.refresh();
-
-        DemoReportFiller demoReportFiller = ctx.getBean(DemoReportFiller.class);
-        demoReportFiller.setReportFileName("statement.jrxml");
-        demoReportFiller.compileReport();
-
-        Map<String, Object> parameters = new HashMap<String, Object>();
-        parameters.put("title", "Employee Report Example");
-        parameters.put("idEmployee", 100);
-
-        demoReportFiller.setParameters(parameters);
-        demoReportFiller.fillReport();
-
-        DemoReportExporter demoExporter = ctx.getBean(DemoReportExporter.class);
-        demoExporter.setJasperPrint(demoReportFiller.getJasperPrint());
-
-        demoExporter.exportToPdf("statement.pdf", "Ravi chandra");
+		SpringApplication.run(JasperdemoApplication.class, args);
 	}
 }
